5ecf3149513cb09201ff093860d852be88ae5560
giorgivawadze <116506482+giorgivawadze@users.noreply.github.com>
2023-04-05 22:06:29 +0400

    update #####

---
+++ Workshop 1/main.py
@@ -0,0 +1,19 @@
+# Task 1
+
+Name = "Giorgi"
+Surname = "Vawadze"
+
+print (Name)
+print (Surname)
+
+
+# Task 2
+
+country = "america"
+
+population ="331.9 million"
+
+print (f'{country.title()}`s population is {population}' )
+
+
+
---
+++ Workshop 3/main.py
@@ -0,0 +1,11 @@
+
+
+eating = True
+khinkhali = 0
+
+while eating:
+  khinkhali+=1
+  print (f'gavskdi {khinkhali}')
+  if khinkhali>=10:
+    eating = False
+   
